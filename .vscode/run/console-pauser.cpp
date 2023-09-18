e76e47460a1d9be786e68521f861950c08d2f762
yezhiyi9670 <yezhiyi9670@sparks-lab.art>
2023-09-18 10:07:04 +0800

    Init

---
+++ .vscode/run/console-pauser.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<string>
+using namespace std;
+
+int main(int argc, char **argv) {
+	if(argc - 1 == 0) {
+		printf("Usage: console-pauser <program> [<arguments...>]\n");
+		return 3;
+	}
+	string cmd = string("\"") + argv[1] + string("\"");
+	for(int i = 2; i < (int)argc; i++) {
+		cmd += string(" ") + "\"" + argv[i] + "\"";
+	}
+
+	int startClock = clock();
+	int ch = system(cmd.c_str());
+	int endClock = clock();
+
+	printf("\n----------------\n");
+	printf("Program finished in %.3lfs with exit code %u\n", double(endClock - startClock) / CLOCKS_PER_SEC, ch);
+}
